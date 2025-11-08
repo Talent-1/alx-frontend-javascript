7a23c7c1db089fa8ddb6e5d5253ea66ac07dda52
Talent-1 <itzmhiztatalent@gmail.com>
2025-11-08 13:16:16 +0100

    feat(task_1): Add Employee factory test

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -36,8 +36,8 @@ class Teacher implements TeacherInterface {
     }
 }
 
-function createEmployee(salary: number | string): Director | TeacherClass {
-    if ( salary < 500) {
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
         return new Teacher();
     }
     return new Director();
